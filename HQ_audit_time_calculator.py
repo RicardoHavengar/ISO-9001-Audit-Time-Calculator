7d66307e6cbd2d2f075ca638cae5337051e0eafe
RicardoHavengar <ricardodesouzaemail@gmail.com>
2025-02-11 07:37:04 -0700

    Feb 10, 2025, commit #5

--- HQ_audit_time_calculator.py
+++ HQ_audit_time_calculator.py
@@ -8,6 +8,7 @@ from iaf_codes import iaf_codes, iaf_codes_table, headers
 hq_audit_time = 0
 total_reduction_percentage = 0
 total_enhancement_percentage = 0
+total_time_adjustment = 0
 
 print("Welcome to the ISO 9001 Audit Time Calculator")
 
@@ -18,7 +19,7 @@ print("EMPLOYEES COUNT")
 print("We will start the calculation of audit days for the organization's Headquarters.")
 hq_employee_count = int(input("Please type the number of employees of the organization's Headquarters:\n"))
 hq_audit_time = find_range_for_employee_count(audit_days_table,hq_employee_count)
-print(f"The audit time according to the IAF MD 5 table for initial certification is: {hq_audit_time}\n")
+print(f"The audit time according to the IAF MD 5 table for initial certification is: {round(hq_audit_time, 2)}\n")
 
 
 print ("NUMBER OF EMPLOYEE PERFORMING SIMILAR ACTIVITIES")
@@ -26,7 +27,7 @@ no_employee_similar_activities = int(input("Please type the number of employees 
 effective_no_employee = hq_employee_count - (no_employee_similar_activities/2)
 print(f"The effective number of employee is: {effective_no_employee}")
 hq_audit_time = find_range_for_employee_count(audit_days_table,effective_no_employee)
-print(f"Based on the effective number of employees, the audit time is {hq_audit_time}")
+print(f"Based on the effective number of employees, the audit time is: {round(hq_audit_time, 2)}\n")
 
 
 
@@ -36,15 +37,16 @@ print(tabulate(iaf_codes_table,headers= headers, tablefmt="grid"))
 selected_iaf_code = int(input("Please type the Number associated to the IAF code related to the scope of "
                               "certification:\n"))
 selected_iaf_code_risk = (iaf_codes[selected_iaf_code]["Risk Level"])
-print (f"The Risk Level for the selected code is: {selected_iaf_code_risk}")
-if selected_iaf_code_risk == "High":
-    hq_audit_time = hq_audit_time +((hq_audit_time*10)/100)
-    total_enhancement_percentage +=10
-else:
-    hq_audit_time = hq_audit_time
-print(total_enhancement_percentage)
-print(f"The audit time has been adjusted to: {hq_audit_time}\n")
 
+print (f"The Risk Level for the selected code is: {selected_iaf_code_risk}")
+# if selected_iaf_code_risk == "High":
+#     hq_audit_time = hq_audit_time +((hq_audit_time*10)/100)
+total_enhancement_percentage +=10
+# else:
+#     hq_audit_time = hq_audit_time
+print(f"The total enhancement of audit time is: {total_enhancement_percentage}%.")
+print(f"The total reduction of audit time is: {total_reduction_percentage}%\n")
+# print(f"The audit time has been adjusted to: {round(hq_audit_time, 2)}\n")
 
 print("DETERMINE THE COMPLEXITY OF THE QMS")
 print("The next 4 questions will help us on defining the Complexity Level of the organization's QMS.")
@@ -66,15 +68,17 @@ else:
     total_reduction_percentage +=10
 print(f"The complexity of the Quality Management System has been determined as: {qms_complexity}\n")
 
-if qms_complexity == "High":
-    hq_audit_time = hq_audit_time +((hq_audit_time*10)/100)
-elif qms_complexity == "Low":
-    hq_audit_time = hq_audit_time - ((hq_audit_time * 10) / 100)
-else:
-    hq_audit_time = hq_audit_time
-print(f"The audit time has been adjusted to: {hq_audit_time}\n")
-print(total_enhancement_percentage)
-print(total_reduction_percentage)
+# if qms_complexity == "High":
+#     hq_audit_time = hq_audit_time +((hq_audit_time*10)/100)
+# elif qms_complexity == "Low":
+#     hq_audit_time = hq_audit_time - ((hq_audit_time * 10) / 100)
+# else:
+#     hq_audit_time = hq_audit_time
+
+print(f"The total enhancement of audit time is: {total_enhancement_percentage}%.")
+print(f"The total reduction of audit time is: {total_reduction_percentage}%")
+# print(f"The audit time has been adjusted to: {round(hq_audit_time, 2)}\n")
+
 
 
 print("DETERMINE THE MATURITY OF THE QUALITY MANAGEMENT SYSTEM")
@@ -98,36 +102,68 @@ else:
     qms_maturity = "Low"
 
 print(f"The maturity of the Quality Management System has been determined as: {qms_maturity}\n")
-if qms_maturity == "High":
-    hq_audit_time = hq_audit_time -((hq_audit_time*10)/100)
-    total_reduction_percentage =+10
-elif qms_maturity == "Low":
-    hq_audit_time = hq_audit_time + ((hq_audit_time * 10) / 100)
-    total_enhancement_percentage =+10
-else:
-    hq_audit_time = hq_audit_time
 
-print(f"The audit time has been adjusted to: {hq_audit_time}\n")
-print(total_enhancement_percentage)
-print(total_reduction_percentage)
+if qms_maturity == "High":
+    # hq_audit_time = hq_audit_time -((hq_audit_time*10)/100)
+    total_reduction_percentage +=10
+elif qms_maturity == "Low":
+    # hq_audit_time = hq_audit_time + ((hq_audit_time * 10) / 100)
+    total_enhancement_percentage +=10
+else:
+    # hq_audit_time = hq_audit_time
+    total_enhancement_percentage = total_enhancement_percentage
+
+
+print(f"The total enhancement of audit time is: {total_enhancement_percentage}%.")
+print(f"The total reduction of audit time is: {total_reduction_percentage}%")
+# print(f"The audit time has been adjusted to: {round(hq_audit_time, 2)}\n")
+
 
 print("OTHER REDUCTIONS")
 
-print("Next, a list of other reason for audit time reduction will be provided. Press" " Enter " "to see the list:")
+def other_reductions():
+    global total_reduction_percentage
+    input("Next, a list of other reason for audit time reduction will be provided. Press" " Enter " "to see the list:")
+    print(f"{(tabulate(reduction_table, headers=reduction_headers, tablefmt="grid"))} \n")
+    other_reduction_selection = int(input ("If you wish to apply further reductions to this calculation, please type below the "
+                                 "related number or type 0 (zero) for no further reductions.\n"
+              "IMPORTANT: The maximum amount of reduction allowed for the calculation is 30%.\n"))
 
-print(f"{(tabulate(reduction_table, headers=reduction_headers, tablefmt="grid"))} \n")
-other_reduction_selection = int(input ("If you wish to apply further reductions to this calculation, please type below the "
-                         "related number.\n"
-      "IMPORTANT: The maximum amount of reduction allowed for the calculation is 30%.\n"))
+    percentage_other_reduction = (reduction_table[other_reduction_selection][2])
+    print(f"The audit time has been reduced in {percentage_other_reduction}%\n")
+    total_reduction_percentage += percentage_other_reduction
+    print(f"The total enhancement of audit time is: {total_enhancement_percentage}%.")
+    print(f"The total reduction of audit time is: {total_reduction_percentage}%")
+    # print(f"The audit time has been adjusted to: {round(hq_audit_time, 2)}\n")
+    if total_reduction_percentage < 30:
+        more_reduction = input("Does any other reduction should be applied? Type (Y/N).\n")
+        if more_reduction =='Y':
+            other_reductions()
+        else:
+                other_enhancements()
+    else:
+        other_enhancements()
 
-percentage_other_reduction = (reduction_table[other_reduction_selection][2])
-print(f"The audit time has been reduced in {percentage_other_reduction}%")
+def other_enhancements():
+    global total_enhancement_percentage
+    global hq_audit_time
+    print("OTHER ENHANCEMENTS")
+    input ("Next, a list of other reason for audit time enhancement will be provided. Press" " Enter " "to see the list:")
+    print(f"{(tabulate(enhancement_table, headers=headers, tablefmt="grid"))} \n")
+    other_enhancement_selection = int(input("If you wish to apply further enhancements to this calculation, "
+                                            "please type below the related number or type 0 (zero) for no further "
+                                            "enhancements.\n"))
+
+    percentage_other_enhancement = (enhancement_table[other_enhancement_selection][2])
+    print(f"the audit time has been enhanced in {percentage_other_enhancement}%\n")
+    total_enhancement_percentage += percentage_other_enhancement
+    print(f"The total enhancement of audit time is: {total_enhancement_percentage}%.")
+    print(f"The total reduction of audit time is: {total_reduction_percentage}%")
+    total_time_adjustment = total_enhancement_percentage - total_reduction_percentage
+    hq_audit_time = hq_audit_time + (hq_audit_time * total_time_adjustment) / 100
+    print(total_time_adjustment)
+    print(f"The audit time has been adjusted to: {round(hq_audit_time, 2)}")
 
 
+other_reductions()
 
-
-print("OTHER ENHANCEMENTS")
-
-input(f"The table below provides a list of enhancements and their respective percentages.\n"
-      f"{(tabulate(enhancement_table, headers=headers, tablefmt="grid"))} \n"
-      "If you wish to apply further enhancements to this calculation, please type below the related number.\n")
